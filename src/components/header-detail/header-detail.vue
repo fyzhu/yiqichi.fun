a0d12faab8925aeb9730eaecddfd1151b76217e8
Yvan <i@zhuyunfeng.com>
2019-09-03 19:04:58 +0800

    feat

--- src/components/header-detail/header-detail.vue
+++ src/components/header-detail/header-detail.vue
@@ -36,13 +36,13 @@
 </template>
 
 <script>
-  // import popupMixin from 'common/mixins/popup'
+  import popupMixin from 'common/mixins/popup'
   import Star from 'components/star/star'
   import SupportIco from 'components/support-ico/support-ico'
 
   export default {
     name: 'header-detail',
-    // mixins: [popupMixin],
+    mixins: [popupMixin],
     props: {
       seller: {
         type: Object,
@@ -51,19 +51,7 @@
         }
       }
     },
-    data() {
-      return {
-        visible: false
-      }
-    },
-    methods: {
-      show() {
-        this.visible = true
-      },
-      hide() {
-        this.visible = false
-      }
-    },
+
     components: {
       SupportIco,
       Star
